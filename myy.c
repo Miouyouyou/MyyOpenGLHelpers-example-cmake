0170b959f711231dd5eec572446e2de430247006
Miouyouyou (Myy) <myy@miouyouyou.fr>
2018-09-20 04:49:44 +0200

    Moved out the platform handling stubs.
    Now, we only have the functions we care about.
    
    The myy.c is clean and simple. The way I like it.
    
    Signed-off-by: Miouyouyou (Myy) <myy@miouyouyou.fr>

--- myy.c
+++ myy.c
@@ -1,22 +1,9 @@
 #include <myy.h>
 #include <myy/current/opengl.h>
 
-struct myy_platform_handlers platform = {
-	.stop = NULL,
-	.stop_data = NULL
-};
-
-struct myy_platform_handlers * myy_get_platform_handlers() {
-	return &platform;
-}
-
 void myy_draw() {
 	glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
 	glClearColor(0.1f, 0.3f, 0.5f, 1.0f);
-}
-
-void myy_user_quit() {
-	platform.stop(platform.stop_data);
 }
 
 void myy_key(unsigned int keycode) {
